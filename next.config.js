eeb3d253be1cac618d68aa0bd51d7822e97d28d1
James Stuart Milne <jstuartmilne@gmail.com>
2023-02-13 14:10:24 -0300

    feat: add workers

--- next.config.js
+++ next.config.js
@@ -1,14 +1,31 @@
-module.exports = {
+const {
+  TaskManager,
+  orkesConductorClient,
+} = require("@io-orkes/conductor-javascript");
+
+const sendEmailWorker = () => {
+  return {
+    taskDefName: "send_email",
+    execute: async ({ inputData }) => {
+      /* const email = inputData?.email; */
+      console.log("SENDING EMAIL TO ",inputData);
+      return {
+        status: "COMPLETED",
+      };
+    },
+  };
+};
+
+const nextConfig = {
   publicRuntimeConfig: {
     conductor: {
       keyId: process.env.KEY,
-      keySecret:process.env.SECRET,
+      keySecret: process.env.SECRET,
       serverUrl: "http://localhost:3456/api",
     },
-    workflows:{
+    workflows: {
       checkout: `${process.env.CHECKOUT_WF_NAME || "MyCheckout2"}`,
-      
-    }
+    },
   },
   async rewrites() {
     return [
@@ -19,4 +36,22 @@ module.exports = {
       },
     ];
   },
+};
+
+module.exports = (phase) => {
+  const playConfig = {
+    keyId: process.env.KEY,
+    keySecret: process.env.SECRET,
+    serverUrl: `${process.env.SERVER_URL}`,
+  };
+
+  (async () => {
+    const clientPromise = orkesConductorClient(playConfig);
+    const client = await clientPromise;
+    const runner = new TaskManager(client, [sendEmailWorker()]);
+    runner.startPolling();
+  })();
+
+  console.log("Starting up ", phase);
+  return nextConfig;
 };
